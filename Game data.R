01650e9b71c073b364050deb644cc4ca424308d8
TheOPCerealBox <oryoncain@gmail.com>
2019-11-04 21:35:43 -0800

    updating chart images

--- Game data.R
+++ Game data.R
@@ -1,109 +1,115 @@
 library(ggplot2)
 
 games <- read.csv("game sales.csv")
+games = read.csv("game sales.csv", na.strings=c("","NA"))
+
+
 head(games)
 
-#Creating scatter plot
-a <- ggplot(data=games, aes(x=Critic_Score, y=User_Score,
-                       colour=Genre, size=Global_Sales)) +
-  geom_point()
+#Clean data
+#Removing blank values
 
-#Adding labels, titles, and formatting
-a +
-  xlab("Critic Score") +
-  ylab("User Score") +
-  ggtitle("User Score vs. Critic Score") +
-  theme(axis.title.x = element_text(size = 12),
-        axis.title.y = element_text(size = 12),
-        plot.title = element_text(size = 15, hjust = 0.5),
-        panel.background = element_rect(fill="lightblue",
-                                        colour = "lightblue"),
-        plot.background = element_rect(fill="gray80"),
-        legend.background = element_rect(bg=))
+#Creating scatter plot
+ggplot(data=games, aes(x=Critic_Score, y=User_Score,
+                       colour=Genre, size=Global_Sales)) +
+  geom_point() +
+    xlab("Critic Score") +
+    ylab("User Score") +
+    ggtitle("User and Critic Ratings") +
+        theme(axis.title.x = element_text(size = 12),
+              axis.title.y = element_text(size = 12),
+              plot.title = element_text(size = 15, hjust = 0.5),
+              panel.background = element_rect(fill="lightblue",
+                                              colour = "lightblue"),
+              plot.background = element_rect(fill="gray83"),
+              legend.background = element_blank())
+
+#2
+ggplot(data=subset(games, !is.na(ESRB_Rating)), aes(x=Year, 
+                                                   y=Global_Sales, colour=ESRB_Rating)) +
+  geom_point() +
+    xlab("Year") +
+    ylab("Sales") +
+    ggtitle("Global Sales") +
+          theme(axis.title.x = element_text(size = 12),
+              axis.title.y = element_text(size = 12),
+              plot.title = element_text(size = 15, hjust = 0.5),
+              panel.background = element_rect(fill="lightblue",
+                                                colour = "lightblue"),
+              plot.background = element_rect(fill="gray83"),
+              legend.background = element_blank())
+
 
 #Creating histograms
 
-ggplot(data=games, aes(x=Global_Sales)) + 
-geom_histogram(aes(fill=Genre), colour = "Black") +
-  xlab("Global Sales (Millions)") +
-  ylab("Count") +
-  ggtitle("Global Sales Distribution") +
-  theme(axis.title.x = element_text(size = 12),
-        axis.title.y = element_text(size = 12),
-        plot.title = element_text(size = 15, hjust = 0.5))
-
-
 ggplot(data=games, aes(x=User_Score)) + 
-  geom_histogram(fill="LightBlue", colour="DarkBlue") +
+  geom_histogram(fill="cornflowerblue", colour="DarkBlue") +
     xlab("User Scores") +
     ylab("Count") +
-           ggtitle("User Ratings") +
+    ggtitle("User Ratings") +
            theme(axis.title.x = element_text(size = 12),
-                 axis.title.y = element_text(size = 12),
-                  plot.title = element_text(hjust =0.5))
+                axis.title.y = element_text(size = 12),
+                plot.title = element_text(hjust =0.5),
+                panel.background = element_rect(fill="lightblue",
+                                                   colour = "lightblue"),
+                plot.background = element_rect(fill="gray83"))
 
 ggplot(data=games, aes(x=Critic_Score)) + 
-  geom_histogram(fill="Coral2", colour="Coral3") +
+  geom_histogram(fill="Coral3", colour="Coral4") +
     xlab("Critic Scores") +
     ylab("Count") +
     ggtitle("Critic Ratings") +
         theme(axis.title.x = element_text(size = 12),
-        axis.title.y = element_text(size = 12),
-        plot.title = element_text(hjust =0.5))           
+            axis.title.y = element_text(size = 12),
+            plot.title = element_text(hjust =0.5),
+            panel.background = element_rect(fill="lightblue",
+                                            colour = "lightblue"),
+            plot.background = element_rect(fill="gray83"))           
 
+#Facets
+ggplot(data=games, aes(x=Critic_Score, y=User_Score, colour=Genre)) +
+  geom_point() +
+    facet_grid(Genre~., scales="free") +
+      xlab("Critic Scores") +
+      ylab("User Rating") +
+      ggtitle("Ratings") +
+        theme(plot.title = element_text(hjust=0.5),
+              panel.background = element_rect(fill="lightblue",
+                                              colour = "lightblue"),
+              plot.background = element_rect(fill="gray83"),
+              legend.background = element_blank())
 
-##Facets
 ggplot(data=games, aes(x=Global_Sales)) +
-         geom_histogram(binwidth=0.6, aes(fill=Genre), 
-                        colour="Black") +
-  facet_grid(Genre~., scales="free") +
-    xlab("Global Sales") +
-    ylab("Count") +
-    ggtitle("Global Sales Distribution") +
-      theme(plot.title = element_text(hjust=0.5))
-
-ggplot(data=games, aes(x=Critic_Score, y=User_Score,
-                            colour=Genre)) +
-  geom_point(size=3) + facet_grid(Genre~., scales="free") +
-    xlab("Critic Score") + 
-    ylab("User Score") +
-    ggtitle("Ratings") +
-      theme(plot.title = element_text(hjust = 0.5))
-
-##ggplot(data=games, aes(x=Critic_Score, y=User_Score,
-##                       colour=Genre)) +
-##  geom_point(size=3) + facet_grid(Year~.)
+  geom_histogram(binwidth=0.1, aes(fill=Genre), colour="Black") +
+    facet_grid(Genre~., scales="free") +
+      xlab("Sales") +
+      ylab("Count") +
+      ggtitle("Global Sales Distribution") +
+      theme(plot.title = element_text(hjust=0.5),
+            panel.background = element_rect(fill="lightblue",
+                                            colour = "lightblue"),
+            plot.background = element_rect(fill="gray83"),
+            legend.background = element_blank())
 
 #Creating boxplot
-bp <- ggplot(data=games, aes(x=Genre, y=User_Score, colour=Genre)) +
-  geom_jitter() + geom_boxplot(size=1.2, alpha=0.5) 
-
-bp + 
-  ylab("User Score") +
-  ggtitle("User Ratings by Genre") +
-    theme(axis.title.x = element_text(size = 12),
-          axis.title.y = element_text(size = 12),
-          plot.title = element_text(size = 15, hjust=0.5))
-
-#Barplot
-ggplot(data=games, aes(x=Publisher, y=Global_Sales)) + geom_bar(stat="identity")
-
-ggplot(data=games, aes(x=Year)) +
-  geom_bar(stat="bin", fill="cyan4", colour="cyan3") +
-    ylab("Count") +
-    ggtitle("Count by Year Released") +
-      theme(plot.title = element_text(hjust = 0.5),
+ggplot(data=games, aes(x=Genre, y=Critic_Score, colour=Genre)) +
+  geom_jitter() + 
+  geom_boxplot(size=1.2, alpha=0.5) +
+    ylab("Critic Scores") +
+    ggtitle("Critic Ratings by Genre") +
+      theme(axis.title.x = element_text(size = 12),
+            axis.title.y = element_text(size = 12),
+            plot.title = element_text(size = 15, hjust=0.5),
             panel.background = element_rect(fill="lightblue",
-                                            colour = "lightblue"))
+                                            colour = "lightblue"),
+            plot.background = element_rect(fill="gray83"),
+            legend.background = element_blank())
+
+
     
-ggplot(data=games, aes(x=factor(Genre))) +
-  geom_bar(stat="count", fill="dodgerblue4", colour="dodgerblue3") +
-  ylab("Count") +
-  xlab("Genre") +
-  ggtitle("Count by Year Released") +
-  theme(plot.title = element_text(hjust = 0.5),
-        panel.background = element_rect(fill="lightblue",
-                                        colour = "lightblue"))
+
+
+
 
 
 
